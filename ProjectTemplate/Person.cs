93f59f213242ddd6e95cc5357a13ae1f6cbaa92d
Carolina <cmurill7@asu.edu>
2023-01-24 18:33:48 -0700

    initial setup

---
+++ ProjectTemplate/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemplate
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int phoneNumber;
+    }
+}
